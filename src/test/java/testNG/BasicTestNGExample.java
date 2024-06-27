0ca8c9125cc5220c534ae80e616b139420dac836
Bidyut Ranjan <bidyut.ranjan93@gmail.com>
2024-06-27 11:56:30 +0530

    fit comments

--- src/test/java/testNG/BasicTestNGExample.java
+++ src/test/java/testNG/BasicTestNGExample.java
@@ -22,7 +22,7 @@ public class BasicTestNGExample {
         driver.get("https://www.flipkart.com/");
         driver.manage().window().maximize();
         System.out.println("Hello Everyone....");
-        
+
     }
 
     @Test
@@ -30,7 +30,6 @@ public class BasicTestNGExample {
 
         driver.get("https://www.amazon.in/");
         driver.manage().window().maximize();
-        System.out.println(" How are you everyone...");
     }
 
     @Test
